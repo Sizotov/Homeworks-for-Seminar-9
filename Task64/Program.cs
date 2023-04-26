d5bb92499d73bf6927698fb4080d6973af09d6eb
Sergey <saizotov1@gmail.com>
2023-04-27 01:38:37 +0500

    Solving the Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -3,3 +3,24 @@
 //числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 //N = 5 -> "5, 4, 3, 2, 1"
 //N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse (ReadLine());
+int count = 1;
+WriteNow (number, count);
+void WriteNow (int n, int count)
+{
+    if (count > n)
+    {
+       return;
+    }
+    else
+    {
+    WriteNow (n, ++count);
+    Write (count+ ", ");
+    }
+
+}
